a61b3255e8366bf98a50c492674e7e982c81a4c5
Siddhrajsinh Parmar <siddhraj.parmar2020@gmail.com>
2025-04-24 22:48:36 -0400

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,14 +10,14 @@ function Footer() {
           <p className="footer-text">
           We are proud to collaborate with the Knowledge Mobilization Office at York University. They have provided invaluable guidance and support, helping us arrange impactful meetings to drive our project forward.
           </p>
-          <img src="c4_logo.png" alt="York University Logo" className="york-logo" />
+          <img src="\c4_logo.png" alt="York University Logo" className="york-logo" />
         </div>
         <div className="footer-right">
           <p className="footer-email">
             <strong>Email:</strong> <a href="mailto:kejensen@yorku.ca">mjohnny@yorku.ca</a>
           </p>
           <p className="footer-address">
-            <strong>Address:</strong> 
+            <strong>Address: </strong> 
             4700 Keele Street, Toronto
             <br />ON M3J 1P3 Canada<br />
           </p>

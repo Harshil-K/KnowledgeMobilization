87b3de9b7349f66dda3ed95b82be75c06dd74cc4
Siddhrajsinh Parmar <siddhraj.parmar2020@gmail.com>
2025-04-24 22:09:00 -0400

    trying to restore footer
    footer things

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import './HeaderFooter.css';
 
-//Add Social Media
 function Footer() {
   return (
-    <footer className="footer">
-      <p>Contact us: kejensen@yorku.ca</p>
-      <p>Follow us on socials: [icons]</p>
+    <footer className="footer enhanced-footer">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h2 className="footer-heading">Our Partners</h2>
+          <p className="footer-text">
+          We are proud to collaborate with the Knowledge Mobilization Office at York University. They have provided invaluable guidance and support, helping us arrange impactful meetings to drive our project forward.
+          </p>
+          <img src="c4_logo.png" alt="York University Logo" className="york-logo" />
+        </div>
+        <div className="footer-right">
+          <p className="footer-email">
+            <strong>Email:</strong> <a href="mailto:kejensen@yorku.ca">mjohnny@yorku.ca</a>
+          </p>
+          <p className="footer-address">
+            <strong>Address:</strong> 
+            4700 Keele Street, Toronto
+            <br />ON M3J 1P3 Canada<br />
+          </p>
+        </div>
+      </div>
     </footer>
   );
 }

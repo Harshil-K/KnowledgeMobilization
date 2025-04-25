5c3cdfde8b23ad234c2601dc329d4df33e653362
Akul Sareen <sareen.akul@gmail.com>
2025-04-24 23:51:32 -0400

    End styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './HeaderFooter.css';
+import "./Navbar.css";
 
 
 //Centre the elements for each page
@@ -8,15 +9,15 @@ function Navbar() {
   return (
     //Add the FAQ link in there.
     <nav className="navbar">
-      <Link to="/">Home</Link>
+      <Link className='titles' to="/">Home</Link>
       {/* <Link to="/guidelines">Guidelines</Link> */}
-      <Link to="/generalresources">General Resources</Link>
-      <Link to="/legalresources">Legal Resources</Link>
-      <Link to="/financialresources">Financial Resources</Link>
-      <Link to="/RAForm">Risk Assessment Form</Link>
-      <Link to="/survey">Survey Results</Link>
-      <Link to="/mission">Our Goals</Link>
-      <Link to="/faq">FAQ</Link>
+      <Link className='titles' to="/generalresources">General Resources</Link>
+      <Link className='titles' to="/legalresources">Legal Resources</Link>
+      <Link className='titles' to="/financialresources">Financial Resources</Link>
+      <Link className='titles' to="/RAForm">Risk Assessment Form</Link>
+      <Link className='titles' to="/survey">Survey Results</Link>
+      <Link className='titles' to="/mission">Our Goals</Link>
+      <Link className='titles' to="/faq">FAQ</Link>
     </nav>
   );
 }
